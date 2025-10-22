79d0f25979f49ef590662500a8382900a7a617ca
Nishanth P Kashyap <113301662+nishu-2004@users.noreply.github.com>
2025-10-22 23:24:58 +0530

    Add change over time analysis SQL script
    This SQL script analyzes sales trends over different time periods, including yearly and monthly sales data, using aggregation and date functions.

---
+++ scripts/07_change_over_time.sql
@@ -0,0 +1,47 @@
+/* 
+  ============================================================
+   Change Over Time Analysis
+   ============================================================
+   Purpose: Analyze sales trends over different time periods 
+            (yearly and monthly) using aggregation and date functions.
+   ============================================================ 
+*/
+--======================================================================
+-- change over time
+-- per duration(year,month) analysis ==> (Can be done using windowing functions or subqueries or basic group by operations)
+
+-- sales per year
+
+select 
+	year(order_date) as order_year,
+	sum(sales_amount) as total_sales,
+	count(distinct customer_key) as total_customers,
+	sum(quantity) as total_quantity
+from gold.fact_sales
+where order_date is not null
+group by year(order_date)
+order by year(order_date) 
+
+
+-- sales per month
+select 
+	year(order_date) as order_year,
+	month(order_date) as order_month,
+	sum(sales_amount) as total_sales,
+	count(distinct customer_key) as total_customers,
+	sum(quantity) as total_quantity
+from gold.fact_sales
+where order_date is not null
+group by year(order_date),month(order_date)
+order by month(order_date),year(order_date)
+
+-- using datetrunc
+select 
+	DATETRUNC(month,order_date) as order_date,
+	sum(sales_amount) as total_sales,
+	count(distinct customer_key) as total_customers,
+	sum(quantity) as total_quantity
+from gold.fact_sales
+where order_date is not null
+group by DATETRUNC(month,order_date)
+order by DATETRUNC(month,order_date)
